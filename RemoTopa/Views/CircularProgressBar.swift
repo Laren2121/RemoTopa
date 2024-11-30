f40f0116bbba2fed82167baabb976ff9b20b6b9c
Laren2121 <Laren2121@users.noreply.github.com>
2024-11-30 17:46:56 -0500

    Feat: Add delayed animation to the progress bar

--- RemoTopa/Views/CircularProgressBar.swift
+++ RemoTopa/Views/CircularProgressBar.swift
@@ -8,34 +8,33 @@
 import SwiftUI
 
 struct CircularProgressBar: View {
-    var progress: Double // Value between 0 and 1
+    var progress: Double
     var lineWidth: CGFloat = 12
     var size: CGFloat = 100
-    var foregroundColor: Color = .white.opacity(0.6)
+    var foregroundColor: Color = .white.opacity(0.8)
     var backgroundColor: Color = .gray.opacity(0.3)
     var animationDuration: Double = 0.5
     var label: String = ""
     var valueLabel: String = ""
 
     @State private var animatedProgress: Double = 0
+    @State private var showForegroundCircle: Bool = false
 
     var body: some View {
         VStack {
             ZStack {
-                // Background Circle
                 Circle()
                     .stroke(lineWidth: lineWidth)
                     .foregroundColor(backgroundColor)
 
-                // Foreground Circle
                 Circle()
                     .trim(from: 0.0, to: animatedProgress)
                     .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
-                    .foregroundColor(.white.opacity(0.8))
+                    .foregroundColor(.white.opacity(0.6))
                     .rotationEffect(Angle(degrees: -90))
+                    .opacity(showForegroundCircle ? 1 : 0)
                     .animation(.easeOut(duration: animationDuration), value: animatedProgress)
 
-                // Value Label
                 Text(valueLabel)
                     .font(.custom("GeneralSans-Extralight", size: 20))
                     .foregroundColor(.white.opacity(0.6))
@@ -43,7 +42,6 @@ struct CircularProgressBar: View {
             .frame(width: size, height: size)
             .padding(.bottom, 15)
 
-            // Label
             Text(label)
                 .font(.custom("GeneralSans-Extralight", size: 14))
                 .foregroundColor(.white.opacity(0.8))
@@ -51,7 +49,14 @@ struct CircularProgressBar: View {
         }
         .padding(20)
         .onAppear {
-            self.animatedProgress = progress
+            // Render background circle first
+            // Delay the rendering and animation of the foreground circle
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                withAnimation(.easeOut(duration: animationDuration)) {
+                    self.showForegroundCircle = true
+                    self.animatedProgress = progress
+                }
+            }
         }
     }
 }
